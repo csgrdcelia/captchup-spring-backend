857727f50a535424d8cfd5e7028be3183c47c540
Daxxxan <alex-bad97@hotmail.fr>
2019-05-05 12:34:16 +0200

    Add find user by id

--- src/main/java/fr/esgi/j2e/group6/captchup/user/web/UserController.java
+++ src/main/java/fr/esgi/j2e/group6/captchup/user/web/UserController.java
@@ -4,10 +4,9 @@ import fr.esgi.j2e.group6.captchup.user.model.User;
 import fr.esgi.j2e.group6.captchup.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path="/user")
@@ -28,5 +27,10 @@ public class UserController {
     @GetMapping(path="/all")
     public @ResponseBody Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping(path="/{id}")
+    public @ResponseBody Optional<User> getUserById(@PathVariable("id") int id) {
+        return userRepository.findById(id);
     }
 }
