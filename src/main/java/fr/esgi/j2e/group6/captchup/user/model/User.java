bd0da73ae0109bf2961dd677e65bedb60a07ed97
csgrdcelia <casagrande.celia@gmail.com>
2019-05-05 20:06:53 +0200

    Implement stateless auth using JWT

--- src/main/java/fr/esgi/j2e/group6/captchup/user/model/User.java
+++ src/main/java/fr/esgi/j2e/group6/captchup/user/model/User.java
@@ -1,18 +1,29 @@
 package fr.esgi.j2e.group6.captchup.user.model;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Collection;
 
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
     private String username;
 
-    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     public Integer getId() {
         return id;
@@ -25,17 +36,42 @@ public class User {
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getEmail() {
-        return email;
+    public String getPassword() {
+        return password;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    // UserDetails methods
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
 
--- src/main/java/fr/esgi/j2e/group6/captchup/user/web/UserController.java
+++ src/main/java/fr/esgi/j2e/group6/captchup/user/web/UserController.java
@@ -3,6 +3,7 @@ package fr.esgi.j2e.group6.captchup.user.web;
 import fr.esgi.j2e.group6.captchup.user.model.User;
 import fr.esgi.j2e.group6.captchup.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,14 +12,22 @@ import java.util.Optional;
 @Controller
 @RequestMapping(path="/user")
 public class UserController {
+
     @Autowired
     private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     @GetMapping(path="/add")
-    public @ResponseBody String addUser(@RequestParam String username, @RequestParam String email) {
+    public @ResponseBody String addUser(@RequestParam String username, @RequestParam String password) {
         User user = new User();
         user.setUsername(username);
-        user.setEmail(email);
+        user.setPassword(password);
         userRepository.save(user);
 
         return "Saved";
@@ -32,5 +41,18 @@ public class UserController {
     @GetMapping(path="/{id}")
     public @ResponseBody Optional<User> getUserById(@PathVariable("id") int id) {
         return userRepository.findById(id);
+    }
+
+    /*@GetMapping(path="/sign-up")
+    public void signUp(@RequestParam String username, @RequestParam String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        userRepository.save(user);
+    }*/
+    @PostMapping(path="/sign-up")
+    public void signUp(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
     }
 }
