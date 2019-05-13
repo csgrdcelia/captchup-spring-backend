17f81c4a34bb3457c74a3bd1b1e380f900e83c76
csgrdcelia <casagrande.celia@gmail.com>
2019-05-13 18:16:57 +0200

    Test getting all users when logged in

--- src/test/java/fr/esgi/j2e/group6/captchup/UserTest.java
+++ src/test/java/fr/esgi/j2e/group6/captchup/UserTest.java
@@ -1,9 +1,12 @@
 package fr.esgi.j2e.group6.captchup;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -12,12 +15,28 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = CaptchupApplication.class)
 @AutoConfigureMockMvc
 public class UserTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    private String token;
+
+    @Before
+    public void setUp() throws Exception {
+        final ResultActions result = mockMvc.perform(
+                post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "\t\"username\": \"testuser\",\n" +
+                                "\t\"password\": \"password\"\n" +
+                                "}")
+        );
+
+        token = result.andReturn().getResponse().getHeader("Authorization");
+    }
 
     @Test
     public void getAllUsers_notLoggedIn_shouldReturnForbidden() throws Exception {
@@ -27,5 +46,31 @@ public class UserTest {
 
         result.andExpect(status().isForbidden());
     }
+
+    @Test
+    public void getAllUsers_LoggedIn_shouldReturnOk() throws Exception {
+        final ResultActions result = mockMvc.perform(
+                get("/user/all")
+                        .header("Authorization",token)
+                        .accept(MimeTypeUtils.APPLICATION_JSON_VALUE));
+
+        result.andExpect(status().isOk());
+    }
+
+    //not tested, wait for delete function
+    /*@Test
+    public void signUp_shouldReturnOk() throws Exception {
+        final ResultActions result = mockMvc.perform(
+          post("/user/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("")
+        );
+
+        result.andExpect(status().isOk());
+
+        //TODO: delete created user
+
+    }*/
+
 
 }
