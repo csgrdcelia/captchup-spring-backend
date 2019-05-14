29fed18980aeebe0d254548c5b90a160ddcd06ae
Vincent Guénin <guenin.vincent4@gmail.com>
2019-05-14 12:16:32 +0200

    Add find by id

--- src/main/java/fr/esgi/j2e/group6/captchup/level/web/LevelController.java
+++ src/main/java/fr/esgi/j2e/group6/captchup/level/web/LevelController.java
@@ -3,10 +3,12 @@ package fr.esgi.j2e.group6.captchup.level.web;
 import fr.esgi.j2e.group6.captchup.level.model.Level;
 import fr.esgi.j2e.group6.captchup.level.repository.LevelRepository;
 import fr.esgi.j2e.group6.captchup.level.service.LevelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/level")
@@ -20,8 +22,21 @@ public class LevelController {
         this.levelService = levelService;
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "/all")
     public @ResponseBody Iterable<Level> getAllLevels() {
         return levelRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody
+    ResponseEntity<Level> getLevelById(@PathVariable("id") int id) {
+        Optional<Level> level = levelRepository.findById(id);
+
+        if(!level.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(level.get());
     }
 }
